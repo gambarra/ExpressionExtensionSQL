b9c13fcb3e458ed74992474ae20f4e9f61818df2
Thiago Gambarra <tgambarra@mundipagg.com>
2018-12-04 19:36:51 -0200

    :art: implementa testes

--- ExpressionExtensionSQL.Tests/SingleExpressionTest.cs
+++ ExpressionExtensionSQL.Tests/SingleExpressionTest.cs
@@ -1,9 +1,7 @@
 ﻿using ExpressionExtensionSQL.Tests.Entities;
 using FluentAssertions;
 using System;
-using System.Collections.Generic;
 using System.Linq.Expressions;
-using System.Text;
 using Xunit;
 
 namespace ExpressionExtensionSQL.Test {
@@ -14,6 +12,13 @@ namespace ExpressionExtensionSQL.Test {
             Expression<Func<Merchant, bool>> expression = x => x.Name == "merchant1";
             var where = expression.ToSql();
             where.Sql.Should().Be("([Merchant].[Name] = @1)");
+        }
+
+        [Fact(DisplayName = "SingleExpression - NotEqual")]
+        public void NotEqualExpression() {
+            Expression<Func<Merchant, bool>> expression = x => x.Name != "merchant1";
+            var where = expression.ToSql();
+            where.Sql.Should().Be("([Merchant].[Name] <> @1)");
         }
 
         [Fact(DisplayName = "SingleExpression - GreaterThan")]
@@ -44,5 +49,18 @@ namespace ExpressionExtensionSQL.Test {
             where.Sql.Should().Be("(NOT ([Product].[Name] LIKE @1))");
         }
 
+        [Fact(DisplayName = "SingleExpression - LessThan")]
+        public void LessThanExpression() {
+            Expression<Func<Product, bool>> expression = x => x.Amount < 10;
+            var where = expression.ToSql();
+            where.Sql.Should().Be("([Product].[Amount] < 10)");
+        }
+
+        [Fact(DisplayName = "SingleExpression - LessThanOrEqual")]
+        public void LessThanOrEqualExpression() {
+            Expression<Func<Product, bool>> expression = x => x.Amount <= 10;
+            var where = expression.ToSql();
+            where.Sql.Should().Be("([Product].[Amount] <= 10)");
+        }
     }
 }
