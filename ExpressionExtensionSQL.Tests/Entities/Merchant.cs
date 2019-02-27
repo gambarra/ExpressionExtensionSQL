27bef43b81a8b583a1a60fa7fc15c51ef80d3032
Felipe Spinelli <felipe.spinelli@mundipagg.com>
2019-02-27 15:52:46 -0300

    :bug: Fixing merge with master problems

--- ExpressionExtensionSQL.Tests/Entities/Merchant.cs
+++ ExpressionExtensionSQL.Tests/Entities/Merchant.cs
@@ -1,6 +1,10 @@
-﻿namespace ExpressionExtensionSQL.Tests.Entities
+﻿using System;
+
+namespace ExpressionExtensionSQL.Tests.Entities
 {
     public class Merchant : Entity {        
         public string Name { get; set; }
+        public bool IsEnabled { get; set; }
+        public DateTime? DeletedAt { get; set; }
     }
 }
